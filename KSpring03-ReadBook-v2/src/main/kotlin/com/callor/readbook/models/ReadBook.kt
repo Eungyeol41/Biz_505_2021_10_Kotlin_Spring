cec3e048dbf8d324b616516dcb8a70b917ebbefd
Eungyeol41 <iniziato@naver.com>
2021-10-29 12:34:37 +0900

    input 이후

---
+++ KSpring03-ReadBook-v2/src/main/kotlin/com/callor/readbook/models/ReadBook.kt
@@ -0,0 +1,19 @@
+package com.callor.readbook.models
+
+data class ReadBook(
+
+    var isbn:String,
+
+    var title:String,
+
+    var sdate:String,
+
+    var stime:String,
+
+    var etime:String,
+
+    var subject:String,
+
+    var content:String,
+
+)
