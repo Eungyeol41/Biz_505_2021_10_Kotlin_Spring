9121a732a0b6bc2f43e643583fb4a48948f74258
Eungyeol41 <iniziato@naver.com>
2021-11-11 07:04:20 +0900

    csrfTokenRepository 추가

--- KSpring03-ReadBook_V4/src/main/kotlin/com/callor/readbook/config/SecurityConfig.kt
+++ KSpring03-ReadBook_V4/src/main/kotlin/com/callor/readbook/config/SecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @EnableWebSecurity
@@ -33,6 +34,16 @@ class SecurityConfig:WebSecurityConfigurerAdapter() {
         http.authorizeRequests()
             .antMatchers("/member/mypage").authenticated()
             .antMatchers("/**").permitAll()
+
+        // 추가
+        // html의 token 때문에 input과 login이 한 번씩 나오지 않는 오류가 있었다.
+        /**
+         * Spring Security의 정책으로 POST Token 설정이 https에만 적용되도록 하였다.
+         *
+         * 아래 옵션을 포함하여 http에서도 적용되도록 설정
+         * 단, 개발 과정에서만 사용하고 실제 https 적용 서버에 배포할 때는 옵션을 제거하는 것이 좋다.
+         */
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 
         http.formLogin()
                 .loginPage("/member/login").permitAll()
